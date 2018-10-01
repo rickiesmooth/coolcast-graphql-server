290acea7b5e5fcd484e66cac475aec3d5111ac16
Rick <rick.smit@ahold.com>
2018-10-01 21:22:32 +0200

    for new deploy

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,14 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { prisma } from './generated/prisma-client'
-import { resolvers } from './resolvers'
+import { GraphQLServer } from "graphql-yoga";
+import { prisma } from "./generated/prisma-client";
+import { resolvers } from "./resolvers";
 
 const server = new GraphQLServer({
-  typeDefs: 'src/schema.graphql',
+  typeDefs: "src/schema.graphql",
   resolvers,
-  context: req => {
-    return {
-      ...req,
-      db: prisma,
-    }
-  },
-} as any)
+  context: req => ({
+    ...req,
+    db: prisma
+  })
+} as any);
 
-server.start(() => console.log('Server is running on localhost:4000'))
+server.start(() => console.log("Server is running on localhost:4000"));
