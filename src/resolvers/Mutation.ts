355c798094849b49a1620b02807d3f470febbbc1
Rick Smit <rick@vigour.io>
2018-10-13 17:43:59 +0200

    started with chat integration

--- src/resolvers/Mutation.ts
+++ src/resolvers/Mutation.ts
@@ -1,40 +1,41 @@
-import { hash, compare } from 'bcrypt'
-import { sign } from 'jsonwebtoken'
-import { MutationResolvers } from '../generated/resolvers'
-import { APP_SECRET } from '../utils'
-import { TypeMap } from './types/TypeMap'
+import { hash, compare } from 'bcrypt';
+import { sign } from 'jsonwebtoken';
+import { MutationResolvers } from '../generated/resolvers';
+import { APP_SECRET } from '../utils';
+import { TypeMap } from './types/TypeMap';
 
 export interface MutationParent {}
 
 export const Mutation: MutationResolvers.Type<TypeMap> = {
-  signup: async (_parent, { password, name, email }, ctx) => {
-    const hashedPassword = await hash(password, 10)
-    const user = await ctx.db.createUser({
-      name,
-      email,
-      password: hashedPassword,
-    })
+    signup: async (_parent, { password, name, email }, ctx) => {
+        const hashedPassword = await hash(password, 10);
+        const user = await ctx.db.createUser({
+            name,
+            email,
+            password: hashedPassword
+        });
 
-    return {
-      token: sign({ userId: user.id }, APP_SECRET),
-      user,
-    }
-  },
-  login: async (_parent, { email, password }, ctx) => {
-    const user = await ctx.db.user({ email })
+        return {
+            token: sign({ userId: user.id }, APP_SECRET),
+            user
+        };
+    },
+    addMessage: async (_parent, { userId, text }, ctx) => {},
+    login: async (_parent, { email, password }, ctx) => {
+        const user = await ctx.db.user({ email });
 
-    if (!user) {
-      throw new Error(`No user found for email: ${email}`)
-    }
+        if (!user) {
+            throw new Error(`No user found for email: ${email}`);
+        }
 
-    const valid = await compare(password, user.password)
-    if (!valid) {
-      throw new Error('Invalid password')
-    }
+        const valid = await compare(password, user.password);
+        if (!valid) {
+            throw new Error('Invalid password');
+        }
 
-    return {
-      token: sign({ userId: user.id }, APP_SECRET),
-      user,
+        return {
+            token: sign({ userId: user.id }, APP_SECRET),
+            user
+        };
     }
-  },
-}
+};
--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -1,14 +1,18 @@
-import { UserResolvers } from '../generated/resolvers'
-import { TypeMap } from './types/TypeMap'
+import { UserResolvers } from '../generated/resolvers';
+import { TypeMap } from './types/TypeMap';
 
 export interface UserParent {
-  id: string
-  email: string
-  name?: string
+    id: string;
+    email: string;
+    name?: string;
+    receivedMessages?: any[];
+    sentMessages?: any[];
 }
 
 export const User: UserResolvers.Type<TypeMap> = {
-  id: parent => parent.id,
-  email: parent => parent.email,
-  name: parent => parent.name,
-}
+    id: parent => parent.id,
+    email: parent => parent.email,
+    name: parent => parent.name,
+    receivedMessages: parent => parent.receivedMessages,
+    sentMessages: parent => parent.sentMessages
+};
