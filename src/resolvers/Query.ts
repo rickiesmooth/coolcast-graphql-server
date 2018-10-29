4375b8bfcf121c5eab2f19480217dedb9bfcb042
Rick <rick.smit@ahold.com>
2018-10-29 12:52:37 +0100

    added resolver to search user

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -5,5 +5,9 @@ import { TypeMap } from "./types/TypeMap";
 export interface QueryParent {}
 
 export const Query: QueryResolvers.Type<TypeMap> = {
-  me: (_parent, _args, ctx) => ctx.db.user({ id: getUserId(ctx) })
+  me: (_parent, _args, ctx) => ctx.db.user({ id: getUserId(ctx) }),
+  searchUser: (_parent, { string }, ctx) =>
+    ctx.db.users({
+      where: { name_contains: string }
+    })
 };
