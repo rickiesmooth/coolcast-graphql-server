a1a26ca76bd6a819a91616020db048e488c4c2b1
Rick <rick.smit@ahold.com>
2018-12-09 21:34:54 +0100

    added resolver to get chat

--- src/resolvers/User.ts
+++ src/resolvers/User.ts
@@ -11,5 +11,20 @@ export const User: UserResolvers.Type<TypeMap> = {
     id: parent => parent.id,
     email: parent => parent.email,
     name: parent => parent.name,
-    chats: (parent, _args, ctx) => ctx.db.user({ id: parent.id }).chats(),
+    chats: (parent, _args, ctx) => {
+        return ctx.db.user({
+            id: parent.id
+        }).chats()
+    },
+    chat: (parent, { userId }, ctx) => {
+        return ctx.db.user({
+            id: parent.id
+        }).chats({
+            where: {
+                users_some: {
+                    id: userId
+                }
+            }
+        })
+    }
 };
