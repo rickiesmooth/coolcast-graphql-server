e914b97d48f36a2b795962be65c7ecfe1c467cf8
Rick <rick.smit@ahold.com>
2018-12-10 09:53:58 +0100

    move resolver to query

--- src/resolvers/Query.ts
+++ src/resolvers/Query.ts
@@ -2,12 +2,19 @@ import { QueryResolvers } from "../generated/resolvers";
 import { getUserId } from "../utils";
 import { TypeMap } from "./types/TypeMap";
 
-export interface QueryParent {}
+export interface QueryParent { }
 
 export const Query: QueryResolvers.Type<TypeMap> = {
   me: (_parent, _args, ctx) => ctx.db.user({ id: getUserId(ctx) }),
   searchUser: (_parent, { string }, ctx) =>
     ctx.db.users({
       where: { name_contains: string }
-    })
+    }),
+  chat: (parent, { userId }, ctx) => ctx.db.user({ id: getUserId(ctx) }).chats({
+    where: {
+      users_some: {
+        id: userId
+      }
+    }
+  })[0]
 };
