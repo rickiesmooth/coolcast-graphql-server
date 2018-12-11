79405d5b1b4ef1d611d321a4608bd55c5fd129eb
Rick <rick.smit@ahold.com>
2018-12-11 20:44:14 +0100

    add messages to chat resolvers

--- src/resolvers/Message.ts
+++ src/resolvers/Message.ts
@@ -4,10 +4,12 @@ import { TypeMap } from './types/TypeMap';
 export interface MessageParent {
     id: string;
     text: string;
+    from: string;
 }
 
 export const Message: MessageResolvers.Type<TypeMap> = {
     id: parent => parent.id,
-    text: parent => parent.text
+    text: parent => parent.text,
+    from: parent => parent.from
 };
 
